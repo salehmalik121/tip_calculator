b0b0c5f4254b0892d8e1236a23df489bfd1d16b7
saleh muhammad <salehmalik121@gmail.com>
2021-08-07 19:26:00 +0500

    practice , practice and practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Calculator());
-}
+void main() => runApp(Calculator());
 
 class Calculator extends StatefulWidget {
   const Calculator({Key? key}) : super(key: key);
@@ -12,103 +10,75 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  final _controller = TextEditingController();
+  List<bool> selection = [false, true, false];
+  String? tip;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Tip Calculator'),
-          leading: Icon(Icons.money_sharp),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (tip != null)
+                Text(
+                  tip.toString(),
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              Text("Total Amount"),
+              SizedBox(
+                width: 80,
+                child: TextField(
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  textAlign: TextAlign.center,
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: "\$100.00",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ToggleButtons(
+                  children: [Text('10%'), Text('15%'), Text('20%')],
+                  isSelected: selection,
+                  onPressed: selected,
+                ),
+              ),
+              TextButton(
+                onPressed: calculateTip,
+                child: Text("Calaculate Tip"),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
+              )
+            ],
+          ),
         ),
-        body: homeColumn(),
-      ),
-    );
-  }
-}
-
-// ignore: camel_case_types
-class homeColumn extends StatefulWidget {
-  const homeColumn({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _homeColumnState createState() => _homeColumnState();
-}
-
-// ignore: camel_case_types
-class _homeColumnState extends State<homeColumn> {
-  String? tip;
-  final controller = TextEditingController();
-  List<bool> _selected = [true, false, false];
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (tip != null)
-            Text(
-              tip.toString(),
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-          Text("Total Amount"),
-          SizedBox(
-            width: 80,
-            child: TextField(
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              textAlign: TextAlign.center,
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: '\$100.00',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ToggleButtons(
-              children: [
-                Text("10%"),
-                Text("15%"),
-                Text("20%"),
-              ],
-              isSelected: _selected,
-              onPressed: selectonFun,
-            ),
-          ),
-          TextButton(
-            onPressed: calcuateTip,
-            child: Text(
-              "Calculate Tip",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.green),
-            ),
-          )
-        ],
       ),
     );
   }
 
-  void selectonFun(int selectedIndex) {
+  void selected(int selectedindex) {
     setState(() {
-      for (int i = 0; i < _selected.length; i++) {
-        _selected[i] = selectedIndex == i;
+      for (int i = 0; i < selection.length; i++) {
+        selection[i] = selectedindex == i;
       }
     });
   }
 
-  calcuateTip() {
-    final amount = double.parse(controller.text);
-    final selectedIndex = _selected.indexWhere((element) => element);
-    final tipPercent = [0.1, 0.15, 0.2][selectedIndex];
-    final tiptotal = (tipPercent * amount).toStringAsFixed(2);
+  calculateTip() {
+    final total = double.parse(_controller.text);
+    final selected = selection.indexWhere((element) => element);
+    final percent = [0.1, 0.15, 0.2][selected];
+    final totalTip = (percent * total).toStringAsFixed(2);
     setState(() {
-      tip = "\$$tiptotal";
+      tip = '\$$totalTip';
     });
   }
 }
